0ed2ab1249f41e0804408e80a436b5c14e11557f
MathuC <mathu99430@hotmail.com>
2025-01-24 16:27:56 -0500

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 kill $(cat app.pid)
 git pull
 npx tsc
-nohup node dist/app.js > app.log 2>&1 &
+nohup node dist/app.js > app.log 2>&1 & # Without '> app.log 2>&1', the workflow job runs forever even if it's nohup
 echo $! > app.pid
 disown
